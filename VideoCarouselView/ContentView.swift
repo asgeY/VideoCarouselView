06cb3e0e059978fffe8a3461a6619d77c597700b
AsgeY <asmow@yahoo.com>
2023-04-25 19:13:58 -0500

    Initial Commit

---
+++ VideoCarouselView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VideoCarouselView
+//
+//  Created by AsgeY on 4/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoCarouselView/VideoCarouselViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCarouselViewApp.swift
+//  VideoCarouselView
+//
+//  Created by AsgeY on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoCarouselViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
